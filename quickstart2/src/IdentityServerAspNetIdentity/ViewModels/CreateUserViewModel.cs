d0b63a2c58f8a47bc5374ebc278073640482a5e0
Miguel <miguel.jalube@gmail.com>
2020-05-29 17:19:42 +0200

    Corrections visuelles

---
+++ quickstart2/src/IdentityServerAspNetIdentity/ViewModels/CreateUserViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityServerAspNetIdentity.ViewModels
+{
+    public class CreateUserViewModel
+    {
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CountryCode { get; set; }
+
+    }
+}
