189656d4a899a8ce0425d4d4286e52f332c41039
miguel <miguel.jalube@gmail.com>
2020-06-09 16:30:37 +0200

    Commit intermediaire pour images

---
+++ quickstart2/src/Api/Controllers/ImageController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageController : ControllerBase
+    {
+        [HttpPost("upload")]
+        public async Task<IActionResult> Post(IFormFile formFile)
+        {
+            long size = formFile.Sum(f => f.Length);
+
+            var uploads = Path.Combine("/", "Images");
+
+            if (formFile.Length > 0)
+            {
+                var filePath = Path.Combine(uploads, formFile.FileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await formFile.CopyToAsync(fileStream);
+                    fileStream.Flush();
+                }
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ quickstart2/src/AppWebClient/Controllers/ImgUploadController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using AppWebClient.Tools;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace AppWebClient.Controllers
+{
+    public class ImgUploadController : Controller
+    {
+        // Var USERID 
+        private readonly string UserID = "002078C2AB";
+
+        // HTTPCLIENT 
+        private HttpClient _client = new HttpClient();
+
+        // URL 
+        private string _url = "api/image/";
+
+        private readonly IConfiguration _configuration;
+
+        public ImgUploadController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> UploadAsync(IFormFile submittedImg)
+        {
+            string accessToken = await HttpContext.GetTokenAsync("access_token");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+            string content = await _client.GetStringAsync(_configuration["URLApi"] + "api/Books/");
+            return View();
+        }
+    }
+}
