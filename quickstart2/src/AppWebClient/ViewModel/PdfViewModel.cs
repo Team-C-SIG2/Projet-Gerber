31b36b1c549d24d231b79b2541a282cf987eb4d7
Marc Vago <marc.vago@bluewin.ch>
2020-06-18 10:26:33 +0200

    Mise en place affichage du paiement en PDF + création ViewModel pour le PDF

---
+++ quickstart2/src/AppWebClient/ViewModel/PdfViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AppWebClient.ViewModel
+{
+    public class PdfViewModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Address { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        public DateTime PaidDate { get; set; }
+        public decimal PriceTotal { get; set; }
+        public string Details { get; set; }
+    }
+}
